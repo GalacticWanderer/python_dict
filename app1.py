c9603bc237e1bbda163f9a8359b1f43f205feacc
joypaul <jpaul3250@gmail.com>
2018-12-11 18:06:17 -0500

    A key/value exercise on python using the basics.

---
+++ app1.py
@@ -0,0 +1,15 @@
+import json  # library to work with json
+
+
+# mehtod to find a value for a given key
+def findADef():
+    word = input("Enter a word: ")
+    word = word.lower()  # converting input to lower case
+    try:
+        data = json.load(open("data.json"))  # loading the data.json file
+        return data[word]  # returning the value associated with the key
+    except KeyError:
+        return "Word doesn't exist"  # return this if key don't exist
+
+
+print(findADef())
